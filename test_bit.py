e35835fe0e1d4cb4b3b8f796b3c7742443b165e2
DionitaShabani <119108890+DionitaShabani@users.noreply.github.com>
2022-12-23 22:55:56 +0100

    Add files via upload

---
+++ test_bit.py
@@ -0,0 +1,40 @@
+
+import streamlit as st
+
+st.title('**:blue[TEST]**      :computer:')
+
+
+
+title = st.text_input('Add your text here 👇:')
+st.write( "You've entered:", title)
+
+def convert_list(title):
+    lst = list(title.split())  #the string is converted to a list
+    return lst
+
+prnt = convert_list(title) 
+if st.button('Return list'):
+    st.write(prnt, 'The string is converted to a list')                  #this button shows the list
+
+
+
+def convert_upper(prnt): #this function converts all elements of list created above to uppercase.
+    upper_list = [] 
+    for i in prnt:
+        upper_list.append(i.upper())
+    return upper_list
+
+prnt_upper = convert_upper(prnt)
+if st.button('Upper'):
+    st.write(prnt_upper, 'All elements of the list are converted to uppercase') #this prints the elements all in uppercase
+
+
+
+def count(prnt):  #counts all elements of the list
+    counter = 0
+    for i in prnt:
+        counter +=1
+    return counter
+prnt_count = count(prnt)
+if st.button('print_count'):
+    st.write('The total number of all elements in the list above is:',prnt_count)
